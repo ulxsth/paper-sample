189a57b92db774db52a12d4482996fbcfdb9616f
ulxsth <ulxsth@gmail.com>
2023-02-03 20:26:46 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.0.0" // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/io/github/yotu/SamplePlugin.kt
@@ -0,0 +1,10 @@
+package io.github.yotu
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SamplePlugin: JavaPlugin() {
+    @Override
+    override fun onEnable() {
+        println("hw")
+    }
+}
